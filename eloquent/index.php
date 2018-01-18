0488998fccb890c95858dbfe6223dfd62add045b
Ruben Engels <ruben.engels@live.nl>
2018-01-18 12:10:06 +0100

    Create index.php

---
+++ eloquent/index.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+// Instantiate the Capsule Manager
+$capsule = new Capsule;
+
+// Add your connection here, in this case, for ease of use a sqlite database file
+$capsule->addConnection([
+	'driver' => 'sqlite',
+	'database' => __DIR__.'/../database.sqlite',
+	'prefix' => ''
+]);
+
+// Set the Event Dispatcher
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Set Eloquent as global
+$capsule->setAsGlobal();
+
+// Boot Eloquent
+$capsule->bootEloquent();
+
+// Create a User model
+class User extends Eloquent {
+
+	protected $fillable = ['name', 'email'];
+
+	public $timestamps = false;
+}
+
+// Create a new table
+Capsule::schema()->create('users', function($table) {
+   $table->increments('id');
+   $table->string('name');
+   $table->string('email');
+});
+
+
+// Fill the table with a record
+User::create([
+	'admin',
+	'admin@admin.nl',
+]);
+
+// Return all of the records from the User model
+dd(User::all())
