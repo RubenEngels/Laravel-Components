0d66997081fec67e2114ad0e525de223d98928e4
Ruben Engels <ruben.engels@live.nl>
2018-01-18 11:26:57 +0100

    Create index.php

---
+++ pagination/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+use Illuminate\Pagination\Paginator;
+use Illuminate
+
+// note, if you want to use the links method you must also use the views component
+Illuminate\Pagination\Paginator::viewFactoryResolver(function () use ("view factory here") {
+  return $factory;
+});
+
+// resolve the current path 
+Paginator::currentPathResolver(function () {
+  return isset($_SERVER['REQUEST_URI']) ? strtok($_SERVER['REQUEST_URI'], '?') : '/';
+});
+
+// Set up a current page resolver
+Paginator::currentPageResolver(function ($pageName = 'page') {
+  $page = isset($_REQUEST[$pageName]) ? $_REQUEST[$pageName] : 1;
+  return $page;
+});
+
+// paginate some data
+$data = User::Paginate(5);
